6be8c717c06fd733e64c1d65105982eb2f9108c1
noiramschneider <117548415+noiramschneider@users.noreply.github.com>
2022-11-05 20:11:45 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let vid;
+let playing = true;
+
+function setup() {
+  createCanvas(1920, 1080);
+  // noCanvas();
+
+  vid = createVideo("flagtest.mp4");
+  vid.size(windowWidth, windowHeight);
+  vid.volume(0);
+  vid.loop();
+  vid.hide(); // hides the html video loader
+  // vid.position(0.0);
+
+}
+
+function draw() {
+  background(220);
+  let img = vid.get();
+  image(img, 0, 0); // redraws the video frame by frame in   
+}
+
